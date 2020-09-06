84377efcf51b80cf6bda926542fbf7ce43288c14
Muhammad Kautsar Apriadi <kautsarady@gmail.com>
2020-09-07 01:04:25 +0700

    Add basic /api

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,16 @@
+import useSWR from 'swr';
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
 export default function Home() {
+  const { data } = useSWR('/api/hello', fetcher);
+
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>
+        Hello
+        {JSON.stringify(data, null, 2)}
+      </h1>
     </div>
   );
 }
