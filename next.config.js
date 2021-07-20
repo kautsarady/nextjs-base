25aa3dd4ac3c37f785725bb992fec802d4bd9a40
Muhammad Kautsar Apriadi <kautsarady@gmail.com>
2021-07-20 23:31:38 +0700

    Use `phase` to check `isDev`

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-const isDev = process.env.NODE_ENV !== 'production';
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 
-module.exports = {
-  distDir: isDev ? '.next' : 'build',
-  pageExtensions: ['jsx', 'js'],
+module.exports = (phase) => {
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER;
+
+  return {
+    distDir: isDev ? '.next' : 'build',
+    pageExtensions: ['jsx', 'js'],
+  };
 };
