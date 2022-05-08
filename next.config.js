7f8849290698219cb4df43d06f9d121f4a9c28ad
Muhammad Kautsar Apriadi <kautsarady@gmail.com>
2022-05-08 18:19:18 +0700

    chore: Upgrade to next 12

--- next.config.js
+++ next.config.js
@@ -4,7 +4,13 @@ module.exports = (phase) => {
   const isDev = phase === PHASE_DEVELOPMENT_SERVER;
 
   return {
+    // Distinguish local dev build folder (.next) with production build folder (build)
     distDir: isDev ? '.next' : 'build',
+
+    // Specify page files extenstion (pages/*.{jsx,js})
     pageExtensions: ['jsx', 'js'],
+
+    // Replacing Terser with SWC for minifying JavaScript
+    swcMinify: true,
   };
 };
